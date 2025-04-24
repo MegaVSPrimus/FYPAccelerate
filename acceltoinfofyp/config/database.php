104b0248923efe50977578f2e99358acddb53da7
KieranMcDonnellSD4 <K00267748@student.tus.ie>
2025-04-24 09:26:14 +0100

    Increase timeout to 5 minutes

--- acceltoinfofyp/config/database.php
+++ acceltoinfofyp/config/database.php
@@ -59,6 +59,8 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+    PDO::ATTR_TIMEOUT => 300, // Increase timeout to 60 seconds
+
             ]) : [],
         ],
 
@@ -148,6 +150,7 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        
         ],
 
         'default' => [
