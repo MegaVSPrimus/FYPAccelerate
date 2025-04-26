1e998406daa78d076871f5d8ec3d1cb52424e2c3
KieranMcDonnellSD4 <K00267748@student.tus.ie>
2025-04-26 11:48:51 +0100

    Added secure route

--- acceltoinfofyp/resources/views/admin/upload.blade.php
+++ acceltoinfofyp/resources/views/admin/upload.blade.php
@@ -86,7 +86,7 @@
 
 <div class="upload-container">
     <h2>Upload an Image</h2>
-    <form action="{{ route('image.store') }}" method="POST" enctype="multipart/form-data">
+    <form action="{{ secure_route('image.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
         <input type="text" name="name" placeholder="Image Name" required>
         <input type="file" name="image" required>
